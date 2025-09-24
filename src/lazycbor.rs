17388ab862634b8c7238caa803a41c33b3ed8690
Le Duc Léandre <leandre.leduc38@gmail.com>
2025-09-24 22:06:59 +0200

    refactor: turned whole common suit_manifest structure to no_std

--- src/lazycbor.rs
+++ src/lazycbor.rs
@@ -1,7 +1,7 @@
+use core::marker::PhantomData;
 use minicbor::{
     Decode, Decoder, Encode, Encoder, decode::Error as DecodeError, encode::Error as EncodeError,
 };
-use once_cell::unsync::OnceCell;
 
 /// Lazy wrapper: on decode, for .bstr cbor T, we store the bytes slice but do NOT decode inner T.
 /// Call `.get()` to decode T later if you need acces to data.
@@ -9,7 +9,7 @@ use once_cell::unsync::OnceCell;
 #[derive(Debug)]
 pub struct LazyCbor<'a, T: 'a> {
     bytes: &'a [u8],
-    cache: OnceCell<T>, // Will store the structure after first get()
+    _ty: PhantomData<&'a T>,
 }
 
 impl<'a, 'bytes: 'a, T, Ctx> Decode<'bytes, Ctx> for LazyCbor<'a, T>
@@ -20,7 +20,7 @@ where
         let bytes: &'bytes [u8] = d.bytes()?;
         Ok(LazyCbor {
             bytes,
-            cache: OnceCell::new(),
+            _ty: PhantomData,
         })
     }
 }
@@ -39,18 +39,15 @@ where
     }
 }
 
-/// Getter to decode inner T when needed. On first call,
-/// it will store the data in cache for next calls
+/// Getter to decode inner T and to get it when needed
 impl<'a, T> LazyCbor<'a, T> {
-    pub fn get(&self) -> Result<&T, DecodeError>
+    pub fn get(&self) -> Result<T, DecodeError>
     where
         T: Decode<'a, ()>,
     {
         // Using OnceCell allows us to init the cache
         // without a &mut self reference (unsafe)
-        self.cache.get_or_try_init(|| {
-            let mut decoder = Decoder::new(self.bytes);
-            T::decode(&mut decoder, &mut ())
-        })
+        let mut decoder = Decoder::new(self.bytes);
+        T::decode(&mut decoder, &mut ())
     }
 }
--- src/suit_decode.rs
+++ src/suit_decode.rs
@@ -2,42 +2,104 @@ use crate::flat_ops::decode_flat_pairs;
 use crate::lazycbor::LazyCbor;
 use crate::suit_cose::*;
 use crate::suit_manifest::*;
-use minicbor::{Decode, Decoder, data::Type, decode::Error as DecodeError, display};
-use regex::Regex;
+use core::str;
+use core::usize;
+use heapless::Vec;
+use minicbor::bytes::ByteSlice;
+use minicbor::{
+    Decode, Decoder,
+    data::Type,
+    decode::{ArrayIterWithCtx, Error as DecodeError},
+};
 
+#[cfg(feature = "alloc")]
+use minicbor::display;
+
+#[cfg(feature = "alloc")]
 impl<'a, T, Ctx> Decode<'a, Ctx> for Debug<T>
 where
     T: Decode<'a, ()>,
 {
     fn decode(d: &mut Decoder<'a>, _: &mut Ctx) -> Result<Self, DecodeError> {
-        println!("Decoding struct: {}", core::any::type_name::<Self>());
-        println!("Decoding with debug...");
+        defmt::info!("Decoding struct: {}", core::any::type_name::<Self>());
+        defmt::info!("Decoding with debug...");
         let bytes = d.input();
-        println!("Shared sequence raw: {}", display(bytes));
+        defmt::info!("Shared sequence raw: {}", display(bytes));
         let inner = T::decode(d, &mut ())?;
         Ok(Debug(inner))
     }
 }
 
+// helper to log with defmt::info! the right Type encountered
+pub fn type_to_str(t: Type) -> &'static str {
+    match t {
+        Type::U8 => "U8",
+        Type::U16 => "U16",
+        Type::U32 => "U32",
+        Type::U64 => "U64",
+        Type::I8 => "I8",
+        Type::I16 => "I16",
+        Type::I32 => "I32",
+        Type::I64 => "I64",
+        Type::F16 => "Float16",
+        Type::F32 => "Float32",
+        Type::F64 => "Float64",
+        Type::Bytes => "Bytes",
+        Type::String => "String",
+        Type::Array => "Array",
+        Type::Map => "Map",
+        Type::Tag => "Tag",
+        Type::Simple => "Simple",
+        Type::Bool => "Bool",
+        Type::Null => "Null",
+        Type::Undefined => "Undefined",
+        Type::Break => "Break",
+        _ => "Unknown",
+    }
+}
+
+impl<'a, T, Ctx, const N: usize> Decode<'a, Ctx> for CborVec<T, N>
+where
+    T: Decode<'a, Ctx>,
+{
+    fn decode(d: &mut Decoder<'a>, ctx: &mut Ctx) -> Result<Self, DecodeError> {
+        let iter: ArrayIterWithCtx<_, T> = d.array_iter_with(ctx)?;
+        let mut v: Vec<T, N> = Vec::new();
+
+        for x in iter {
+            let item = x?;
+            v.push(item).map_err(|_item| {
+                defmt::error!("Too many items in a heapless Vec");
+                DecodeError::message("Too many items in a Vec")
+            })?;
+        }
+
+        Ok(CborVec(v))
+    }
+}
+
 impl<'a> Decode<'a, ()> for SuitStart<'a> {
-    fn decode(d: &mut Decoder<'a>, _ctx: &mut ()) -> Result<Self, minicbor::decode::Error> {
+    fn decode(d: &mut Decoder<'a>, _ctx: &mut ()) -> Result<Self, DecodeError> {
         match d.tag()?.as_u64() {
-            107 => Ok(SuitStart::EnvelopeTagged(Box::new(
+            107 => Ok(SuitStart::EnvelopeTagged(
                 d.decode_with::<(), SuitEnvelope>(_ctx)?,
-            ))),
-            1070 => Ok(SuitStart::ManifestTagged(Box::new(
+            )),
+            1070 => Ok(SuitStart::ManifestTagged(
                 d.decode_with::<(), SuitManifest>(_ctx)?,
-            ))),
+            )),
             0 => Ok(SuitStart::Start),
-            other => Err(minicbor::decode::Error::message(format!(
-                "unexpected tag {other}"
-            ))),
+            other => {
+                defmt::error!("SuitStart: unexpected tag: {:?}", other);
+                Err(minicbor::decode::Error::message(
+                    "unexpected tag for SuitStart",
+                ))
+            }
         }
     }
 }
 
 impl<'a, Ctx> Decode<'a, Ctx> for SuitAuthenticationBlock {
-    fn decode(d: &mut Decoder<'a>, _ctx: &mut Ctx) -> Result<Self, minicbor::decode::Error> {
+    fn decode(d: &mut Decoder<'a>, _ctx: &mut Ctx) -> Result<Self, DecodeError> {
         match d.tag()?.as_u64() {
             98 => Ok(SuitAuthenticationBlock::Sign(
                 d.decode_with::<Ctx, CoseSign>(_ctx)?,
@@ -52,14 +114,17 @@ impl<'a, Ctx> Decode<'a, Ctx> for SuitAuthenticationBlock {
                 d.decode_with::<Ctx, CoseMac0>(_ctx)?,
             )),
 
-            other => Err(minicbor::decode::Error::message(format!(
-                "unexpected tag {other}"
-            ))),
+            other => {
+                defmt::error!("SuitAuthenticationBlock: unexpected tag: {:?}", other);
+                Err(minicbor::decode::Error::message(
+                    "unexpected tag for SuitAuthenticationBlock",
+                ))
+            }
         }
     }
 }
 
-impl<'a, Ctx, T> Decode<'a, Ctx> for DigestOrCbor<T>
+impl<'a, Ctx, T> Decode<'a, Ctx> for DigestOrCbor<'a, T>
 where
     T: Decode<'a, Ctx>,
 {
@@ -79,19 +144,23 @@ where
                 let digest = SuitDigest::decode(d, _ctx)?;
                 Ok(DigestOrCbor::Digest(digest))
             }
-
-            other => Err(DecodeError::message(format!(
-                "unexpected type for DigestOrCbor: {other:?} (expected bstr.cbor or Digest array)"
-            ))),
+            other => {
+                defmt::error!(
+                    "SuitAuthenticationBlock: unexpected type: {:?}",
+                    type_to_str(other)
+                );
+                Err(minicbor::decode::Error::message(
+                    "unexpected type for SuitAuthenticationBlock",
+                ))
+            }
         }
     }
 }
-
 // We implement this to decode because the shared sequence is flat encoded
 // it means [key, value, key, value] instead of [[key,value],[key, value]]
 impl<'a, Ctx> Decode<'a, Ctx> for SuitSharedSequence<'a> {
     fn decode(d: &mut Decoder<'a>, _ctx: &mut Ctx) -> Result<Self, DecodeError> {
-        let mut items: Vec<SharedSequenceItem<'a>> = Vec::new();
+        let mut items: Vec<SharedSequenceItem<'a>, SUIT_MAX_ARRAY_LENGTH> = Vec::new();
 
         // handler closure called for each op; it must consume the argument.
         decode_flat_pairs(d, |op, dec| {
@@ -99,27 +168,25 @@ impl<'a, Ctx> Decode<'a, Ctx> for SuitSharedSequence<'a> {
                 // Commands
                 12 => {
                     let idx = IndexArg::decode(dec, _ctx)?;
-                    items.push(SharedSequenceItem::Command(Box::new(
+                    items.push(SharedSequenceItem::Command(
                         SuitSharedCommand::SetComponentIndex(idx),
-                    )));
+                    ));
                 }
                 32 => {
                     let seq = LazyCbor::<SuitSharedSequence>::decode(dec, _ctx)?;
-                    items.push(SharedSequenceItem::Command(Box::new(
-                        SuitSharedCommand::RunSequence(seq),
+                    items.push(SharedSequenceItem::Command(SuitSharedCommand::RunSequence(
+                        seq,
                     )));
                 }
                 15 => {
                     let arg = SuitDirectiveTryEachArgumentShared::decode(dec, _ctx)?;
-                    items.push(SharedSequenceItem::Command(Box::new(
-                        SuitSharedCommand::TryEach(arg),
-                    )));
+                    items.push(SharedSequenceItem::Command(SuitSharedCommand::TryEach(arg)));
                 }
                 20 => {
                     let params = SuitParameters::decode(dec, _ctx)?;
-                    items.push(SharedSequenceItem::Command(Box::new(
-                        SuitSharedCommand::OverrideParameters(Box::new(params)),
-                    )));
+                    items.push(SharedSequenceItem::Command(
+                        SuitSharedCommand::OverrideParameters(params),
+                    ));
                 }
 
                 // Conditions (all consume SuitRepPolicy)
@@ -139,14 +206,15 @@ impl<'a, Ctx> Decode<'a, Ctx> for SuitSharedSequence<'a> {
                 }
 
                 other => {
-                    return Err(DecodeError::message(format!(
-                        "unknown shared-sequence op id {other}"
-                    )));
+                    defmt::error!("unknow SharedSequence op id: {:?}", other);
+                    return Err(minicbor::decode::Error::message(
+                        "unknow SharedSequence op id",
+                    ));
                 }
             }
             Ok(())
         })?;
-        Ok(SuitSharedSequence(items))
+        Ok(SuitSharedSequence(CborVec(items)))
     }
 }
 
@@ -183,19 +251,22 @@ impl<'a, Ctx> minicbor::Decode<'a, Ctx> for IndexArg {
                         vec.push(d.u64()?);
                     }
                 }
-                Ok(IndexArg::Multiple(vec))
+                Ok(IndexArg::Multiple(CborVec(vec)))
             }
 
-            other => Err(minicbor::decode::Error::message(format!(
-                "unexpected type for IndexArg: {other:?}"
-            ))),
+            other => {
+                defmt::error!("IndexArg: unexpected type: {:?}", type_to_str(other));
+                Err(minicbor::decode::Error::message(
+                    "unexpected type for IndexArg",
+                ))
+            }
         }
     }
 }
 
 impl<'a, Ctx> Decode<'a, Ctx> for SuitCommandSequence<'a> {
     fn decode(d: &mut Decoder<'a>, _ctx: &mut Ctx) -> Result<Self, DecodeError> {
-        let mut items: Vec<SuitCommand> = Vec::new();
+        let mut items: Vec<SuitCommand, SUIT_MAX_ARRAY_LENGTH> = Vec::new();
         decode_flat_pairs(d, |op, dec| {
             match op {
                 // Conditions
@@ -256,7 +327,7 @@ impl<'a, Ctx> Decode<'a, Ctx> for SuitCommandSequence<'a> {
                 20 => {
                     let params = SuitParameters::decode(dec, _ctx)?;
                     items.push(SuitCommand::Directive(SuitDirective::OverrideParameters(
-                        Box::new(params),
+                        params,
                     )));
                 }
                 21 => {
@@ -282,43 +353,32 @@ impl<'a, Ctx> Decode<'a, Ctx> for SuitCommandSequence<'a> {
                     items.push(SuitCommand::Custom(v));
                 }
 
-                unknown => {
-                    return Err(DecodeError::message(format!(
-                        "unknown command op id {unknown}"
-                    )));
+                other => {
+                    defmt::error!("unknow SuitCommandSequence op id: {:?}", other);
+                    return Err(minicbor::decode::Error::message(
+                        "unknow SharedSequence op id",
+                    ));
                 }
             }
             Ok(())
         })?;
-        Ok(SuitCommandSequence { item: items })
+        Ok(SuitCommandSequence {
+            item: CborVec(items),
+        })
     }
 }
 
-impl<'a, Ctx> minicbor::Decode<'a, Ctx> for CommandCustomValue {
+impl<'a, Ctx> minicbor::Decode<'a, Ctx> for CommandCustomValue<'a> {
     fn decode(
         d: &mut minicbor::Decoder<'a>,
         _ctx: &mut Ctx,
     ) -> Result<Self, minicbor::decode::Error> {
         use minicbor::data;
 
-        // debug helper
-        eprintln!(
-            "CommandCustomValue: pos={:?}, next={:?}, remaining={}",
-            d.position(),
-            d.datatype()?,
-            minicbor::display(d.input())
-        );
-
         match d.datatype()? {
-            data::Type::Bytes => {
-                let v = d.bytes()?.to_vec();
-                Ok(CommandCustomValue::Bytes(v))
-            }
+            data::Type::Bytes => Ok(CommandCustomValue::Bytes(d.bytes()?)),
 
-            data::Type::String => {
-                let s = d.str()?;
-                Ok(CommandCustomValue::Text(s.to_string()))
-            }
+            data::Type::String => Ok(CommandCustomValue::Text(d.str()?)),
 
             data::Type::U8 | data::Type::U16 | data::Type::U32 | data::Type::U64 => {
                 let v = d.u64()? as i64;
@@ -335,9 +395,15 @@ impl<'a, Ctx> minicbor::Decode<'a, Ctx> for CommandCustomValue {
                 Ok(CommandCustomValue::Nil)
             }
 
-            other => Err(minicbor::decode::Error::message(format!(
-                "unexpected type for CommandCustomValue: {other:?}"
-            ))),
+            other => {
+                defmt::error!(
+                    "CommandCustomValue: unexpected type: {:?}",
+                    type_to_str(other)
+                );
+                Err(minicbor::decode::Error::message(
+                    "unexpected type for CommandCustomValue",
+                ))
+            }
         }
     }
 }
@@ -346,28 +412,33 @@ impl<'a, Ctx> minicbor::Decode<'a, Ctx> for CommandCustomValue {
 pub fn decode_uuid_or_cborpen<'a, Ctx>(
     d: &mut Decoder<'a>,
     _ctx: &mut Ctx,
-) -> Result<Option<Vec<u8>>, DecodeError> {
+) -> Result<Option<&'a ByteSlice>, DecodeError> {
     match d.datatype()? {
         minicbor::data::Type::Tag => {
             let t = d.tag()?;
             if t.as_u64() == 112 {
                 let b = d.bytes()?;
-                let mut uuid = Vec::with_capacity(16);
-                uuid.extend_from_slice(b);
-                Ok(Some(uuid))
+                if b.len() > 16 {
+                    defmt::error!("The UUID is too long (more than 16 bytes)");
+                    Err(minicbor::decode::Error::message(
+                        "expected UUID or cbor-pen got other type",
+                    ))
+                } else {
+                    Ok(Some(d.bytes().map(<&ByteSlice>::from)?))
+                }
             } else {
                 Err(minicbor::decode::Error::message(
                     "expected tag 112 for cbor-pen",
                 ))
             }
         }
-        minicbor::data::Type::Bytes => {
-            let b = d.bytes()?;
-            Ok(Some(b.to_vec()))
+        minicbor::data::Type::Bytes => Ok(Some(d.bytes().map(<&ByteSlice>::from)?)),
+        other => {
+            defmt::error!("expected UUID or cbor-pen, got {:?}", type_to_str(other));
+            Err(minicbor::decode::Error::message(
+                "expected UUID or cbor-pen got other type",
+            ))
         }
-        other => Err(minicbor::decode::Error::message(format!(
-            "expected UUID or cbor-pen, got {other:?}"
-        ))),
     }
 }
 
@@ -378,14 +449,54 @@ impl<'a, Ctx> Decode<'a, Ctx> for SuitReportingBits {
     }
 }
 
-// Only accept regex matching tags
-impl<'a, C> Decode<'a, C> for Tag38LTag {
+// Only accept valid  tags (if working with owned tag should be at least more than 43 caract)
+impl<'a, C> Decode<'a, C> for Tag38LTag<'a> {
     fn decode(d: &mut Decoder<'a>, _: &mut C) -> Result<Self, DecodeError> {
-        let tag = String::from_utf8_lossy(d.bytes()?);
-        let re = Regex::new(r"^[a-zA-Z]{1,8}(-[a-zA-Z0-9]{1,8})*$").unwrap();
-        if re.is_match(&tag) {
-            return Ok(Tag38LTag(tag.into_owned()));
+        let tag_bytes = d.bytes()?;
+        if is_valid_tag38ltag(tag_bytes) {
+            Ok(Tag38LTag(str::from_utf8(&tag_bytes).map_err(|e| {
+                defmt::error!("Utf8 error for tag38ltag at: {:?}", e.valid_up_to());
+                DecodeError::message("Utf8 parsing error for Tag38LTag")
+            })?))
+        } else {
+            Err(DecodeError::message("Invalid tag38-ltag format"))
         }
-        Err(DecodeError::message("Invalid tag38-ltag format"))
     }
+}
+// Helper to assert  [a-zA-Z]{1,8}(-[a-zA-Z0-9]{1,8})* tag format (Regex not possible in no_std)
+fn is_valid_tag38ltag(bytes: &[u8]) -> bool {
+    let mut i = 0;
+    let len = bytes.len();
+
+    let mut count = 0;
+    while i < len && bytes[i].is_ascii_alphabetic() {
+        i += 1;
+        count += 1;
+        if count > 8 {
+            return false;
+        }
+    }
+    if count == 0 {
+        return false;
+    }
+
+    while i < len {
+        if bytes[i] != b'-' {
+            return false;
+        }
+        i += 1;
+        count = 0;
+        while i < len && (bytes[i].is_ascii_alphanumeric()) {
+            i += 1;
+            count += 1;
+            if count > 8 {
+                return false;
+            }
+        }
+        if count == 0 {
+            return false;
+        }
+    }
+
+    true
 }
